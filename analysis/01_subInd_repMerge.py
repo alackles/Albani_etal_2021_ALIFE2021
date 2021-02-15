3a01408bd6128a380789f464228149fd3a26818d
Acacia Ackles <alackles@msu.edu>
2021-02-15 15:56:19 -0500

    Add merging script

---
+++ analysis/01_subInd_repMerge.py
@@ -0,0 +1,125 @@
+############################
+# repMerge.py
+#
+# Author: Acacia Ackles
+#
+# Purpose:
+# This script outputs a single long data format file of merged replicates and conditions.
+#############################
+
+
+######## Code #############
+
+# import required modules
+
+import pandas as pd
+import csv
+import glob
+
+# constants, filenames, and other things like that
+
+# conditions
+brains = ["Markov", "RNN"] # brains
+mgates = {"ann": "MDA_0__MAA_1", "det": "MDA_1__MAA_0"} #markov: gates
+mht = ["0", "1"] # markov: discretizing
+rwr = ["01010", "11111", "10201"] # rnn: weight biasing towards/away from 0
+rdr = ["1", "-1", "5"] # rnn: discretization
+
+first_rep = 101
+last_rep = 103
+reps = [str(x) for x in range(first_rep, last_rep+1)]
+
+lod_data_filename = "LOD_data.csv"
+lod_org_filename = "LOD_organisms.csv"
+max_filename = "max.csv"
+pop_filename = "pop.csv"
+
+#ss_org_filename = "snapshot_organisms_10.csv"
+#ss_data_filename = "snapshot_data_10.csv"
+base_datapath = "../data/"
+
+# list of the columns that we want to keep
+df_columns = ["update","ID","score_AVE"]
+#genome_col = "GENOME_root::_sites" # this is the nasty default column name for the genome :(
+
+# time to do the merging
+
+def merge_my_file(filename):
+    if filename == pop_filename:
+        df_columns.remove("ID")
+    merged_file = pd.DataFrame(columns=df_columns)
+    for brain in brains:
+        for rep in reps:
+            # find the files and make sure everything's fine
+            gatetype = "NA"
+            threshhold = "NA"
+            weighting = "NA"
+            discretize = "NA"
+            if brain == "Markov":
+                for gate, code in mgates.items():
+                    for ht in mht: 
+                        gatetype = gate
+                        threshhold = ht
+                        globpath = base_datapath + "conditions/C*" + "BRN_" + brain + "__" + code + "__" + "MHT_" + ht + "/" + rep + "/"
+                        datapath = glob.glob(globpath + filename)
+                        if len(datapath) == 1:
+                            datapath = "".join(datapath)
+                            # if everything's fine, open up the files
+                            with open(datapath) as f:
+                                print(datapath)
+                                filemerge = pd.read_csv(f, sep=",", usecols=df_columns)
+                        elif not len(datapath):
+                            print("No files matched.")
+                            print("Path: ", globpath)
+                            break
+
+                        # let's merge them into one tidy file to output for later
+                        filemerge["rep"] = rep
+                        filemerge["brain"] = brain
+                        filemerge["gatetype"] = gatetype
+                        filemerge["threshhold"] = threshhold
+                        filemerge["weighting"] = weighting
+                        filemerge["discretize"] = discretize
+                        # rename the other columns to be sensible
+                        filemerge = filemerge.rename(
+                            columns={"score_AVE": "score"})
+
+                        # add to our list of dataframes for each k
+                        merged_file = merged_file.append(filemerge)
+            elif brain == "RNN":
+                for wr in rwr:
+                    for dr in rdr: 
+                        weighting = wr
+                        discretize = dr
+                        globpath = base_datapath + "conditions/C*" + "BRN_" + brain + "__" + "RWR_" + wr + "__" + "RDR_" + dr + "/" + rep + "/"
+                        datapath = glob.glob(globpath + filename)
+                        if len(datapath) == 1:
+                            datapath = "".join(datapath)
+                            # if everything's fine, open up the files
+                            with open(datapath) as f:
+                                print(datapath)
+                                filemerge = pd.read_csv(f, sep=",", usecols=df_columns)
+                        elif not len(datapath):
+                            print("No files matched.")
+                            print("Path: ", globpath)
+                            break
+
+                        # let's merge them into one tidy file to output for later
+                        filemerge["rep"] = rep
+                        filemerge["brain"] = brain
+                        filemerge["gatetype"] = gatetype
+                        filemerge["threshhold"] = threshhold
+                        filemerge["weighting"] = weighting
+                        filemerge["discretize"] = discretize
+                        # rename the other columns to be sensible
+                        filemerge = filemerge.rename(
+                            columns={"score_AVE": "score"})
+
+                        # add to our list of dataframes for each k
+                        merged_file = merged_file.append(filemerge)
+    filepath = base_datapath + "merged_" + filename
+    merged_file.to_csv(filepath,index=False)
+
+files = [lod_data_filename, max_filename, pop_filename]
+for fname in files:
+    merge_my_file(fname)
